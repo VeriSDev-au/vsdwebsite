96cd212ddf0727ddf92ba9e6dde006fe95f89b6a
Veri Sukawiratmo <veris@verisdev.au>
2023-03-28 17:08:12 +1100

    Bug Fix Enhancement for hosting in Railway.App 8

--- vsd_website/settings/production.py
+++ vsd_website/settings/production.py
@@ -8,7 +8,7 @@ STATIC_URL = "static/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/static"),
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
