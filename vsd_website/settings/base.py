ab85a8ebd9e512cef467219df7ad70172bc1157e
VeriSDev-au <verisdev@gmail.com>
2024-07-14 11:36:24 +1000

    Add runtime.txt

--- vsd_website/settings/base.py
+++ vsd_website/settings/base.py
@@ -23,15 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("VSD_SECRET_KEY")
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "web-production-71b7.up.railway.app",
-    "verisdev.au",
-    "www.verisdev.au"
-]
-
-
 CSRF_TRUSTED_ORIGINS = [
     "https://web-production-71b7.up.railway.app",
     "https://verisdev.au",
